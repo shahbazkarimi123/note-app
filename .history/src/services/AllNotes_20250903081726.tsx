24aec553162ac3b1a880e3d0db9d88df1a4b6a86
shahbazkarimi123 <shahbazkarimi123@gmail.com>
2025-09-03 08:18:26 +0530

    url changed

--- .history/src/services/AllNotes_20250903081726.tsx
+++ .history/src/services/AllNotes_20250903081726.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_BASE_URL = "https://note-app-backend-production-assignment.up.railway.app/"
+const API_BASE_URL = "https://note-app-backend-production-assignment.up.railway.app/
 export const getAllNotes = async ()=>{
     try{
         const response = await axios.get(`${API_BASE_URL}/all`);
