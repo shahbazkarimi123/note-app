e3d518f62b3a99bed97cb6e90baf8d9e2544e3a3
shahbazkarimi123 <shahbazkarimi123@gmail.com>
2025-09-03 08:27:04 +0530

    api/notes line added. left before

--- .history/src/services/AllNotes_20250903082602.tsx
+++ .history/src/services/AllNotes_20250903082602.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_BASE_URL = "https://note-app-backend-production-assignment.up.railway.app/api/notes"
+const API_BASE_URL = "https://note-app-backend-production-assignment.up.railway.app/a"
 export const getAllNotes = async ()=>{
     try{
         const response = await axios.get(`${API_BASE_URL}/all`);
